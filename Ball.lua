136b43237e6369db4146e10a69fee04fa839a311
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-02-20 09:51:33 +0700

    add sounds, serving state, win state
    Add sound and game over

--- Ball.lua
+++ Ball.lua
@@ -1,30 +1,37 @@
 Ball = Class{}
 
-function Ball:init(x, y, width, height)
+function Ball:init(x, y, radius)
     self.x = x;
     self.y = y;
-    self.width = width;
-    self.height = height;
-
+    -- self.width = width;
+    -- self.height = height;
+    self.radius = radius
     self.dx = math.random(2) == 1 and 100 or -100;
     self.dy = math.random(-50, 50)* 1.5;
 
 end
 
-function Ball:update(dt)
+function Ball:update(dt) 
+   
     self.x = self.x + self.dx*dt
     self.y = self.y + self.dy*dt 
 end
 
 function Ball:reset()
-    self.x = VIRTUAL_WIDTH/2 -2;
-    self.y = VIRTUAL_HEIGH/2 -2;
+    self.x = VIRTUAL_WIDTH/2;
+    self.y = VIRTUAL_HEIGH/2;
 
     self.dx = math.random(2) == 1 and 100 or -100;
-    self.dy = math.random(-50, 50)* 1.5;
+    self.dy = math.random(-50, 50) * 1.5;
+end
+
+function Ball:collides(paddle)
+    return (self.x - self.radius <= paddle.x + paddle.width) and (self.x + self.radius >= paddle.x) 
+            and (self.y - self.radius <= paddle.y + paddle.height) and (self.y + self.radius >= paddle.y)
 end
 
 function Ball:render()
-    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+    -- love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+    love.graphics.circle('fill', self.x , self.y, self.radius)
 end
 
--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ WINDOW_HEIGHT = 720;
 VIRTUAL_WIDTH = 432;
 VIRTUAL_HEIGH = 243;
 
-PADDLE_SPEED = 200;
+PADDLE_SPEED = 150;
 
 function love.load()
     -- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -34,7 +34,11 @@ function love.load()
     smallFont = love.graphics.newFont('font.ttf', 8)
     largeFont = love.graphics.newFont('font.ttf', 32)
 
-    
+    sounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static')
+    }
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGH, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false;
         resizable = false;
@@ -53,8 +57,10 @@ function love.load()
 
     -- ball_DX = math.random(2) == 1 and 100 or -100;
     -- ball_DY = math.random(-50, 50)* 1.5;
-    ball = Ball(VIRTUAL_WIDTH/2 -2, VIRTUAL_HEIGH/2 -2, 4, 4)
+    ball = Ball(VIRTUAL_WIDTH/2, VIRTUAL_HEIGH/2, 2)
     game_state = 'start';
+    serve_player = math.random(2);
+    winning_player = 0;
 end
 
 function love.keypressed(key)
@@ -64,17 +70,57 @@ function love.keypressed(key)
 
     if key == 'enter' or key == 'return' then
         if game_state == 'start' then
+            game_state = 'serving';
+        elseif game_state == 'serving' then
             game_state = 'play';
+        elseif game_state == 'game_over' then
+            player1_score = 0;
+            player2_score = 0;
+            serve_player = math.random(2);
+            game_state = 'serving'
         else
             game_state = 'start';
-            ball:reset();
-        
+            ball:reset(); 
         end
     end
 end
 
 function love.update(dt)
+    if game_state == 'serving' then 
+        ball.dy = math.random(-50,50)
+        if serve_player == 1 then
+            ball.dx = math.random(50,150)
+        else 
+            ball.dx = -math.random(50,150)
+        end
+    end
+
     if game_state == 'play' then
+
+        if ball:collides(player1) then
+            ball.dx = -ball.dx*1.03
+            ball.x = player1.x + 7
+
+            if ball.dy < 0 then 
+                ball.dy = - math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+            sounds['paddle-hit']:play();
+        end
+        
+        if ball:collides(player2) then
+            ball.dx = -ball.dx*1.03
+            ball.x = player2.x - 3
+
+            if ball.dy < 0 then 
+                ball.dy = - math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+            sounds['paddle-hit']:play();
+        end
+
         if love.keyboard.isDown('w') then
             player1.speed = -PADDLE_SPEED
         elseif love.keyboard.isDown('s') then
@@ -91,6 +137,34 @@ function love.update(dt)
             player2.speed = 0
         end
         ball:update(dt);
+        if ball.y + ball.dy*dt  <= ball.radius or ball.y + ball.dy*dt >= VIRTUAL_HEIGH - ball.radius then
+            ball.dy = -ball.dy
+            sounds['wall-hit']:play();
+        end
+
+        if ball.x < ball.radius then 
+            player2_score = player2_score + 1
+            if player2_score == 10 then
+                game_state = 'game_over'
+                winning_player = 2
+            else
+                game_state = 'serving'
+                serve_player = 1
+            end
+            ball:reset()
+            sounds['score']:play()
+        elseif ball.x > VIRTUAL_WIDTH - ball.radius then
+            player1_score = player1_score + 1
+            if player1_score == 10 then
+                game_state = 'game_over'
+                winning_player = 1
+            else
+                game_state = 'serving'
+                serve_player = 2
+            end
+            ball:reset()
+            sounds['score']:play()
+        end
     end
 
     player1:update(dt);
@@ -101,8 +175,16 @@ function love.draw()
     push:apply('start')
 
     -- love.graphics.clear(40, 45, 52, 255)
-    love.graphics.setFont(smallFont)
-    love.graphics.printf("Hello Pong", 0, 20, VIRTUAL_WIDTH, 'center');
+    if game_state == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf("Hello Pong", 0, 20, VIRTUAL_WIDTH, 'center');
+    elseif game_state == 'serving' then
+        love.graphics.printf("Player " .. tostring(serve_player) .. "'s serve!" , 0, 20, VIRTUAL_WIDTH, 'center');
+        love.graphics.printf("Press Enter to serve", 0, 40, VIRTUAL_WIDTH, 'center');
+    elseif game_state == 'game_over' then 
+        love.graphics.printf("Player " .. tostring(winning_player) .. "' win!" , 0, 20, VIRTUAL_WIDTH, 'center');
+        love.graphics.printf("Press Enter to start", 0, 40, VIRTUAL_WIDTH, 'center');
+    end
 
     love.graphics.setFont(largeFont)
     love.graphics.print(tostring(player1_score), VIRTUAL_WIDTH/2 - 50, VIRTUAL_HEIGH/3);
